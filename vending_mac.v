852f386891022ade5d786e6c3b06c0d61fe8d960
srujana30-106 <139633665+srujana30-106@users.noreply.github.com>
2024-04-21 17:09:27 +0530

    Add files via upload

---
+++ vending_mac.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+module vending_mac(input clk,
+input rst,
+input [1:0]in,//01=5rs 10=10rs
+output reg out,
+output reg [1:0] change
+    );
+
+
+	 parameter state0=2'b00;
+	 parameter state1=2'b01;
+	 parameter state2=2'b10;
+ 
+reg [1:0] prsnt_state, nxt_state;
+ 
+ always @(posedge clk or posedge rst)
+	begin
+		if(rst)
+			begin
+			 prsnt_state=state0;
+			 nxt_state=state0;
+			 change=2'b00;
+			 out=0;
+			end
+		else
+		begin
+			 prsnt_state=nxt_state;
+				case(prsnt_state)
+				state0:begin
+					if(in==2'b00) 
+						 begin 
+						 nxt_state=state0;
+						 out=0;
+						 change =0;
+						 end
+					else if(in==2'b01)
+						 begin
+						 nxt_state=state1;
+						 out=0;
+						 change=2'b00;
+						 end
+					else 
+						 begin
+						 nxt_state=state2;
+						 out=0;
+						 change=2'b00;
+						 end
+						 end
+			 state1:begin
+					if(in==2'b00)
+						 begin
+						 nxt_state=state0;
+						 out=0;
+						 change=2'b01;
+						 end
+					else if(in==2'b10)
+						 begin
+						 nxt_state=state0;
+						 out=1;
+						 change=2'b00;
+						 end
+					else 
+						 begin
+						 nxt_state=state2;
+						 out=0;
+						 change=2'b00;
+						 end
+						 end
+			 state2:begin
+					if(in==2'b00)
+						 begin
+						 nxt_state=state0;
+						 out=0;
+						 change=2'b10;
+						 end
+					else if(in==2'b10)
+						 begin
+						 nxt_state=state0;
+						 out=1;
+						 change=2'b01;
+						 end
+					else 
+						 begin
+						 nxt_state=state0;
+						 out=1;
+						 change=2'b00;
+						 end
+						 end
+			 endcase
+	end
+	end
+endmodule
---
+++ vending_tb.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+
+module vend;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg [1:0] in;
+
+	// Outputs
+	wire out;
+	wire [1:0] change;
+
+	// Instantiate the Unit Under Test (UUT)
+	vending_mac uut (
+		.clk(clk), 
+		.rst(rst), 
+		.in(in), 
+		.out(out), 
+		.change(change)
+	);
+
+	initial begin
+		rst=1;
+		clk=0;
+		
+		#5rst=0;
+		#100 in=2'b01;
+		#150 in=2'b10;
+		#50 in=2'b10;
+		#100 in =2'b00;
+		#100 in=2'b01;
+		#100 in=2'b01;
+		
+
+	end
+	always #5 clk=~clk;
+      
+endmodule
+
