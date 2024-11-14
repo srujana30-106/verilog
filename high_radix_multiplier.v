e40ff1aa4fce46b6702df41d87542afe3f6b012f
srujana30-106 <139633665+srujana30-106@users.noreply.github.com>
2024-11-14 22:16:25 +0530

    Update high_radix_multiplier.v

--- high_radix_multiplier.v
+++ high_radix_multiplier.v
@@ -1,67 +1,62 @@
 `timescale 1ns / 1ps
 
-
 module high_radix_multiplier;
 
-	
-reg [15:0] x,y;
-wire [31:0] out;
-reg clk,reset;
-high_radix x1 (clk,reset,x,y,out);
+    reg [15:0] x, y;
+    wire [31:0] out;
+    reg clk, reset;
+    high_radix x1 (clk, reset, x, y, out);
 
-	initial
-	begin
-		clk=1;
-	forever #5 clk=~clk;
-	end
+    initial begin
+        $monitor("At time %t: x = %d, y = %d, out = %d", $time, x, y, out);
+    end
 
-	initial
-	begin
-		reset=1;
-		#20;
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(1, high_radix_multiplier);
+        #1000;  // End simulation after some time
+        $finish;
+    end
 
-		reset=0;
-		x=255;
-		y=255;
-		#100
+    initial begin
+        clk = 1;
+        forever #5 clk = ~clk;
+    end
 
-		reset=1;
-		#20
+    initial begin
+        reset = 1;
+        #20;
 
-		reset=0;
-		x=128;
-		y=128;
-		#100
+        reset = 0;
+        x = 255; y = 255;
+        #100;
 
+        reset = 1;
+        #20;
 
-		x=128;
-		y=0;
-		#100
+        reset = 0;
+        x = 128; y = 128;
+        #100;
 
+        x = 128; y = 0;
+        #100;
 
-		x=128;
-		y=1;
-		#100
+        x = 128; y = 1;
+        #100;
 
+        x = 25; y = 5;
+        #100;
 
-		x=25;
-		y=5;
-		#100
+        x = 64; y = 64;
+        #100;
 
-		x=64;
-		y=64;
-		#100
+        x = 36; y = 36;
+        #100;
 
-		x=36;
-		y=36;
-		#100
+        x = 11; y = 33;
+        #100;
 
-		x=11;
-		y=33;
-		#100
-
-		x=80;
-		y=10;
-		#100;
-	end
+        x = 80; y = 10;
+        #100;
+    end
 endmodule
